42fc562af90af40ad065fe72840721292aec7842
mAnic <arctic.manic@gmail.com>
2021-02-14 19:01:53 +0300

    add routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainPage from '../main-page/main-page';
+import MainEmptyPage from '../main-empty-page/main-empty-page';
+import LoginPage from '../login-page/login-page';
+import FavoritesPage from '../favorites-page/favorites-page';
+import FavoritesEmptyPage from '../favorites-empty-page/favorites-empty-page';
+import PropertyPage from '../property-page/property-page';
+import PropertyNotLoggedPage from '../property-not-logged-page/property-not-logged-page';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 const App = (props) => {
   const {placesCount} = props;
 
-  return <React.Fragment>
-    <MainPage placesCount={placesCount} />
-  </React.Fragment>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage placesCount={placesCount} />
+        </Route>
+        <Route exact path="/dev-main-empty">
+          <MainEmptyPage />
+        </Route>
+        <Route exact path="/dev-favorites-empty">
+          <FavoritesEmptyPage />
+        </Route>
+        <Route exact path="/dev-property-not-logged">
+          <PropertyNotLoggedPage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route exact path="/offer/:id">
+          <PropertyPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <React.Fragment>
+      <div className="page page--gray page--main">
+        <header className="header">
+          <div className="container">
+            <div className="header__wrapper">
+              <div className="header__left">
+                <Link className="header__logo-link header__logo-link--active" to="/">
+                  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+                </Link>
+              </div>
+              <nav className="header__nav">
+                <ul className="header__nav-list">
+                  <li className="header__nav-item user">
+                    <a className="header__nav-link header__nav-link--profile" href="#">
+                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+        <main className="page__main">
+          <div className="container">
+            <h1>404 Not Found</h1>
+          </div>
+        </main>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NotFoundPage;
